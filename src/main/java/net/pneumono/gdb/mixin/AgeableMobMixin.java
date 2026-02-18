49c7ca63f78f00357a18833f9f4ddde26bdb0714
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2026-02-18 06:02:52 +0000

    Fix golden dandelions not being usable again to unlock aging

--- src/main/java/net/pneumono/gdb/mixin/AgeableMobMixin.java
+++ src/main/java/net/pneumono/gdb/mixin/AgeableMobMixin.java
@@ -2,12 +2,23 @@ package net.pneumono.gdb.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.level.Level;
+import net.pneumono.gdb.AgeLockData;
+import net.pneumono.gdb.GDBRegistry;
 import net.pneumono.gdb.GDBUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AgeableMob.class)
-public abstract class AgeableMobMixin {
+public abstract class AgeableMobMixin extends PathfinderMob {
+    protected AgeableMobMixin(EntityType<? extends PathfinderMob> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @ModifyExpressionValue(
             method = "aiStep",
             at = @At(
@@ -18,5 +29,17 @@ public abstract class AgeableMobMixin {
     private boolean preventAging(boolean original) {
         // IntelliJ says this will always be false but lowkey I don't believe it
         return original && !GDBUtil.isAgeLocked((AgeableMob)(Object)this);
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    @Inject(
+            method = "aiStep",
+            at = @At("HEAD")
+    )
+    private void tickAgeLockCooldown(CallbackInfo ci) {
+        AgeLockData data = getAttached(GDBRegistry.AGE_LOCK_DATA);
+        if (data != null && data.ageLockCooldown() > 0) {
+            setAttached(GDBRegistry.AGE_LOCK_DATA, new AgeLockData(data.ageLocked(), data.ageLockCooldown() - 1));
+        }
     }
 }
